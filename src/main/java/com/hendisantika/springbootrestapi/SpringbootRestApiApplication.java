7f08917215e955d0dfa38354fc4a278c6854aa56
hendisantika <hendisantika@yahoo.co.id>
2019-03-15 06:33:53 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootrestapi/SpringbootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestApiApplication.class, args);
+    }
+
+}
