8fc5fdbabdd0c63171bcb8271708e3caf2589c8f
hendisantika <hendisantika@yahoo.co.id>
2019-03-15 06:41:53 +0700

    #7 - Add Person Controller

--- src/main/java/com/hendisantika/springbootrestapi/controller/PersonController.java
+++ src/main/java/com/hendisantika/springbootrestapi/controller/PersonController.java
@@ -1,5 +1,15 @@
 package com.hendisantika.springbootrestapi.controller;
 
+import com.hendisantika.springbootrestapi.entity.Person;
+import com.hendisantika.springbootrestapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Optional;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : springboot-rest-api
@@ -9,5 +19,38 @@ package com.hendisantika.springbootrestapi.controller;
  * Date: 2019-03-15
  * Time: 06:38
  */
+@RestController
+@RequestMapping("/people")
 public class PersonController {
+
+    @Autowired
+    private PersonRepository personRepo;
+
+    @GetMapping
+    public ResponseEntity<Collection<Person>> getPeople() {
+        return new ResponseEntity<>(personRepo.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPerson(@PathVariable long id) {
+        Optional<Person> person = personRepo.findById(id);
+
+        if (person != null) {
+            return new ResponseEntity<>(personRepo.findById(id).get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addPerson(@RequestBody Person person) {
+        return new ResponseEntity<>(personRepo.save(person), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePerson(@PathVariable long id) {
+        personRepo.deleteById(id);
+
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
 }
