0963fd8b5fbe58d4ce904f12b3e886717826d481
hendisantika <hendisantika@yahoo.co.id>
2019-03-15 07:19:40 +0700

    #16 - Modify data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,21 @@ INSERT INTO people (person_id, name, age) VALUES
 	('1', 'Naruto', 25),
 	('2', 'Sasuke', 30),
 	('3', 'Sakura', 28);
+
+INSERT INTO skills (skill_id, person_id, name, level) VALUES
+	(1, 1, 'Juggling', 'GOOD'),
+	(2, 1, 'Dancing', 'AWESOME'),
+	(3, 2, 'Juggling', 'AWESOME'),
+	(4, 2, 'Story-telling', 'GODLIKE'),
+	(5, 3, 'Singing', 'GOOD');
+
+INSERT INTO parties (party_id, location, party_date) VALUES
+	(1, 'Old Folks Club', '2016-09-20'),
+	(2, 'Luxury Yacht Party', '2016-12-05');
+
+INSERT INTO people_parties (person_id, party_id) VALUES
+	(1, 1),
+	(1, 2),
+	(2, 1),
+	(3, 2);
+
