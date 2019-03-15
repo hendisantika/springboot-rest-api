9271cf76842935beeb5048f6aebd539d38de9da0
hendisantika <hendisantika@yahoo.co.id>
2019-03-16 06:29:02 +0700

    #24 - Add User Service

---
+++ src/main/java/com/hendisantika/springbootrestapi/service/UserService.java
@@ -0,0 +1,30 @@
+package com.hendisantika.springbootrestapi.service;
+
+import com.hendisantika.springbootrestapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-16
+ * Time: 06:24
+ */
+@Service("userDetailsService")
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return userRepository.findOneByUsername(username);
+    }
+}
