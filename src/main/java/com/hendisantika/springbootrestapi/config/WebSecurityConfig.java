13dbb351cf046e371e14e7b1984ba35192f5f285
hendisantika <hendisantika@yahoo.co.id>
2019-04-18 07:44:07 +0700

    #39 - Add WebSecurityConfig

---
+++ src/main/java/com/hendisantika/springbootrestapi/config/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.hendisantika.springbootrestapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-18
+ * Time: 07:43
+ */
+@Configuration
+@EnableWebSecurity
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * Constructor disables the default security settings
+     */
+    public WebSecurityConfig() {
+        super(true);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/login");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/gigy/**")
+                .authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
